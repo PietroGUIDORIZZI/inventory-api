5e7d95590595c419945070823e36993649343f7d
PietroBIN <peterguidorizzi@gmail.com>
2026-05-20 11:35:07 -0300

    Created Item model

--- src/main/java/com/pietro/inventory_api/model/Item.java
+++ src/main/java/com/pietro/inventory_api/model/Item.java
@@ -1,4 +1,55 @@
 package com.pietro.inventory_api.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private int quantity;
+
+    public Item() {
+    }
+
+    public Item(String name, String description, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
