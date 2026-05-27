d7dc83045920d31e6582f3452222b11f01f02d35
PietroBin <peterguidorizzi@gmail.com>
2026-05-27 16:02:35 -0300

    Implemented ErrorResponse according to best market practices.

--- src/main/java/com/pietro/inventory_api/exception/ErrorResponse.java
+++ src/main/java/com/pietro/inventory_api/exception/ErrorResponse.java
@@ -4,22 +4,46 @@ import java.time.LocalDateTime;
 
 public class ErrorResponse {
 
+    private LocalDateTime timestamp;
 
+    private Integer status;
+
+    private String error;
 
     private String message;
 
-    private int status;
+    private String path;
 
-    public ErrorResponse(String message, int status) {
-        this. message = message;
+    public ErrorResponse(
+            Integer status,
+            String error,
+            String message,
+            String path
+    ) {
+        this.timestamp = LocalDateTime.now();
         this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
     }
 
-    public String getMessage(){
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
         return message;
     }
 
-    public int getStatus() {
-        return status;
+    public String getPath() {
+        return path;
     }
 }
